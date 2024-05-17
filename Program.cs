8253589314b3419f8ff6c8a14a5d4c2f75d07601
Lecs0 <156198766+Lecs0@users.noreply.github.com>
2024-05-17 19:30:45 +0200

    Added all of the Files,Source code
    Use it as you want! | Originaly made by ebola man on YT Check him out he is a cool guy

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RATZ_IPLocator
+{
+
+    public class Data
+    {
+        public string ip { get; set; }
+        public string timezone { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public string postal { get; set; }
+        public string loc { get; set; }
+    }
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "IPLocator";
+            Console.Write("Enter IP address: ");
+            string IP = Console.ReadLine();
+            string URL = $"https://ipinfo.io/{IP}/json";
+
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(URL);
+                    response.EnsureSuccessStatusCode();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[+] Http request made");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("---------------------------|");
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    Data ipInfo = JsonConvert.DeserializeObject<Data>(responseData);
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"ip: {ipInfo.ip}");
+                    Console.WriteLine($"Timezone: {ipInfo.timezone}");
+                    Console.WriteLine($"City: {ipInfo.city}");
+                    Console.WriteLine($"Region: {ipInfo.region}");
+                    Console.WriteLine($"Country: {ipInfo.country}");
+                    Console.WriteLine($"Postal: {ipInfo.postal}");
+                    Console.WriteLine($"loc: {ipInfo.loc}");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("---------------------------|");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error msg: {ex.Message}");
+                Console.WriteLine("Error message logged successfully.");
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Made by Lecs0");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Track GeoLocation via IP adress
